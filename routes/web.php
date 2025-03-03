eee1c8efa7836d4a0e93ad999eceba2b4ed6efe4
amrthyb <3130022011@student.unusa.ac.id>
2025-03-03 12:07:17 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('heloo', function () {
+    return 'hello word';
+});
+
+Route::get('/user/{id}', function ($id) {
+    return 'User ' . $id;
 });
